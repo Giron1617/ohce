e566309d7a5695d196880e9a94520f1476672bf6
Giron1617 <juan.giron@ucp.edu.co>
2024-04-12 19:44:51 -0400

    Se crea el primer test en rojo

---
+++ test_ohce.py
@@ -0,0 +1,19 @@
+"Permite gestionar y ejecutar pruebas unitarias. -->"
+import unittest
+
+class testohce(unittest.TestCase):
+
+    def test_salu_6_12(self):
+
+        """
+    se definen los test que comprueban el resultado esperado de las funciones en 'ohce.py',
+    atravez de la funcion 'self.assertEqual(lo que enviamos, lo que esperamos recibir)'. -->  
+    
+        """
+
+        nombre = "juan"
+        horas = 8
+
+        resu = ohce.salu(nombre, horas)
+
+        self.assertEqual(resu, "buenos dias juan")
